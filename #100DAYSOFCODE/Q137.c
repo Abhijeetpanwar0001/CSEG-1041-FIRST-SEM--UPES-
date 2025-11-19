797aa516eb0576cde60d0d3e8e79e72abba4273f
Abhijeetpanwar0001 <abhiyant3355panwar@gmail.com>
2025-11-19 20:34:55 +0530

    Create Q137.c

---
+++ #100DAYSOFCODE/Q137.c
@@ -0,0 +1,34 @@
+// Create an enum for user roles (ADMIN, USER, GUEST) and display messages based on role.
+#include <stdio.h>
+
+enum Role {
+    ADMIN,
+    USER,
+    GUEST
+};
+
+int main() {
+    enum Role role;
+
+    
+    role = ADMIN;   
+
+    switch (role) {
+        case ADMIN:
+            printf("Welcome Admin! You have full access.\n");
+            break;
+
+        case USER:
+            printf("Hello User! You have limited access.\n");
+            break;
+
+        case GUEST:
+            printf("Hi Guest! Please login to access more features.\n");
+            break;
+
+        default:
+            printf("Invalid role.\n");
+    }
+
+    return 0;
+}
